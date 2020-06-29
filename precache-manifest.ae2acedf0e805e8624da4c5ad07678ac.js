b13eda01f4e330723e70f7a01a3fe5a9efa78139
catriont <catriona.torklep@gmail.com>
2020-06-29 13:46:44 +0200

    Updates

---
+++ precache-manifest.ae2acedf0e805e8624da4c5ad07678ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab8e0ee2b7e705649447d945017abbf",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "d78a5f873d9fae21bbd3",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b647f1dd6a48686c0aa1",
+    "url": "/infoskjerm/static/js/2.efe507bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.efe507bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78a5f873d9fae21bbd3",
+    "url": "/infoskjerm/static/js/main.c0ced2f3.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
